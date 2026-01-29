6977daddf3b39b6fccf1f280b67327bdbf274691
terenceb-uofa <tbedell@ualberta.ca>
2026-01-29 01:27:45 +0000

    Heart Shape class

---
+++ app/src/main/java/com/example/photon/Heart.java
@@ -0,0 +1,15 @@
+package com.example.photon;
+
+public class Heart extends Shape{
+    //---Variables
+    private int curvature;
+    
+    //---Constructors
+    public Heart(int x, int y, int curvature){
+        this.x = x;
+        this.y = y;
+        this.curvature = curvature;
+    }
+    //---Methods
+}
+
