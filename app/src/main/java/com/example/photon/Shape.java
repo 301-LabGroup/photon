2fa6684bb0b64b02e4682866f1e050f105080613
hassanali161 <shali1@ualberta.ca>
2026-01-28 18:10:55 -0700

    made the shape class

---
+++ app/src/main/java/com/example/photon/Shape.java
@@ -0,0 +1,13 @@
+package com.example.photon;
+
+public abstract class Shape {
+    public int x;
+    public int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
